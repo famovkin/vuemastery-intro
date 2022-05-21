fe7de17f81c125b137771a611a2857fa1b0b3056
Tolya Fam <famovkin@yandex.ru>
2022-05-21 12:14:56 +0300

    create base structure

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <h1>Vue Mastery</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'App',
+  components: {
+  },
+});
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 20px;
+}
+</style>
