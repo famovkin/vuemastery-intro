64d3d4a82221c4c2606b255c99f31520b22927aa
Tolya Fam <famovkin@yandex.ru>
2022-05-21 16:23:37 +0300

    fix: add some styles to App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <my-header title="Vue mastery"></my-header>
+    <my-header title="Vue mastery"></my-header>
+    <div class="app__container">
+    </div>
 </template>
 
 <script lang="ts">
@@ -19,6 +21,7 @@ export default defineComponent({
   margin: 0;
   padding: 0;
   box-sizing: border-box;
+  list-style: none;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
@@ -26,5 +29,14 @@ export default defineComponent({
 
 body {
   min-width: 320px;
+  background: $background;
+}
+
+.app__container {
+  padding: 0px 40px;
+
+  @media (max-width: 425px) {
+    padding: 0px 10px;
+  }
 }
 </style>
